cf8c952413d2279053befa847968aa86cd34ba40
Anup490 <anupbhandari490@gmail.com>
2024-11-17 11:30:07 +0530

    Add project files.

---
+++ Main/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Engine.h"
+
+int main()
+{
+	test();
+	return 0;
+}
---
+++ VulkanEngine/Engine.cpp
@@ -0,0 +1,13 @@
+// VulkanEngine.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+
+#include "vulkan/vulkan.h"
+
+void test()
+{
+	std::cout << "Called Engine.cpp/test function" << std::endl;
+}
